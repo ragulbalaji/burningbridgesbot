2177f762e7b3b4397a8c8a1321aef9bfb6998fd1
Ragul Balaji <ragulbaby@gmail.com>
2016-08-10 00:29:27 +0800

    stats!

--- server.js
+++ server.js
@@ -10,6 +10,44 @@ var bot = new TelegramBot(token, {
 var users = {};
 var games = {};
 
+// START of STATISTICSjs
+var stats = {
+    totalPlays: 0,
+    lastupdatetime: Date.now(),
+};
+
+loadStats();
+
+function loadStats() {
+    fs.readFile("assets/stats.txt", function(err, data) {
+        if (err) {
+            console.error(err);
+        }
+        console.log("Stats LOADED >> " + data)
+        stats = JSON.parse(data);
+    });
+}
+
+function saveStats() {
+    stats.lastupdatetime = Date.now();
+    var save = JSON.stringify(stats)
+    console.log("Stats SAVED >> " + save)
+    fs.writeFile("assets/stats.txt", save);
+}
+
+function saveQuestions(qn) {
+    console.log("Question SAVED >> " + qn)
+    fs.appendFile("assets/questionsasked.txt", Date.now().toString() + " : " + qn.toString() + "\n");
+}
+
+function oneMoreTotalPlay() {
+    stats.totalPlays++;
+    saveStats()
+}
+
+// END of STATISTICSjs
+
+
 function shuffle(a) {
     var j, x, i;
     for (i = a.length; i; i--) {
@@ -49,12 +87,14 @@ bot.onText(/^\/startbb$/, function(msg, match) {
     }
     var user = msg.from;
     var chat = msg.chat;
-    bot.sendMessage(msg.chat.id, "Game started by *" + compose_name(user) + "*. Type /joinbb to participate 👻", {"parse_mode":"Markdown"});
+    bot.sendMessage(msg.chat.id, "Game started by *" + compose_name(user) + "*. Type /joinbb to participate 👻", {
+        "parse_mode": "Markdown"
+    });
     games[msg.chat.id] = games[msg.chat.id] || {}; //init game
     games[msg.chat.id].active = 1; //game is active
     games[msg.chat.id].players = {}; //array of players
 
-	 games[msg.chat.id].players[msg.from.id] = msg.from; // add this dude in first
+    games[msg.chat.id].players[msg.from.id] = msg.from; // add this dude in first
 });
 
 bot.onText(/^\/joinbb(.*)$/, function(msg, match) {
@@ -75,7 +115,9 @@ bot.onText(/^\/joinbb(.*)$/, function(msg, match) {
     games[msg.chat.id].players[msg.from.id] = msg.from;
     var user = msg.from;
     var chat = msg.chat;
-    bot.sendMessage(chat.id, "*" + compose_name(user) + "* joined", {"parse_mode":"Markdown"});
+    bot.sendMessage(chat.id, "*" + compose_name(user) + "* joined", {
+        "parse_mode": "Markdown"
+    });
 });
 
 
@@ -93,60 +135,75 @@ bot.onText(/^\/forcestart$/, function(msg, match) {
     var chat = msg.chat;
     var user = msg.user;
     var players = Object.keys(games[msg.chat.id].players);
-    if (players.length < 1) { // rmb to change to < 3
-        bot.sendMessage(msg.chat.id, 'Need > 2 players! Imma not let you play with ' + players.length);
+    if (players.length < 1) { // Rmb to change to < 2
+        bot.sendMessage(msg.chat.id, 'Need > 1 players! 😈 Imma not let you play with ' + players.length);
         return;
     }
     games[chat.id].playerorder = players;
     shuffle(players);
     var playerorder = "";
     for (var i = 0; i < players.length; i++) {
-        playerorder += (i+1).toString()+". *" + compose_name(games[chat.id].players[players[i]]) + "*\n";
+        playerorder += (i + 1).toString() + ". *" + compose_name(games[chat.id].players[players[i]]) + "*\n";
     }
-    bot.sendMessage(msg.chat.id, "Game Starting, Ordered Players are: \n" + playerorder, {"parse_mode":"Markdown"});
-    
+    bot.sendMessage(msg.chat.id, "Game Starting, Ordered Players are: \n" + playerorder, {
+        "parse_mode": "Markdown"
+    });
+
+    oneMoreTotalPlay() //Stats
 
     games[chat.id].counter = 0; //start at 0
-       var opts = {
-  reply_markup: JSON.stringify(
-    {
-      force_reply: true
-    }
-  )};
+    var opts = {
+        reply_markup: JSON.stringify({
+            force_reply: true
+        })
+    };
 
-    bot.sendMessage(players[0], 'Ask <...> a Question! Note, you need to reply to this message', opts) //ask the question
-    .then(function (sent) {
-      var chatId = sent.chat.id;
-      var messageId = sent.message_id;
-      console.log(chatId, messageId);
-      bot.onReplyToMessage(chatId, messageId, function (message) {
-        bot.sendMessage(chatId, 'You asked \'' + message.text + '\'');
-        bot.sendMessage(players[0], '<...> asked \'' + message.text + '\''); //TODO, REMEMBER TO SEND TO THE NEXT PLAYER
-        var playeroptions = [];
-        for(var i = 0;i<players.length; i++ )
-        {
-            playeroptions.push([ {text: compose_name(games[chat.id].players[players[i]]), callback_data: players[i]}]);
-        }
-	console.log(playeroptions);
-        bot.sendMessage(players[0], 'Point to: ', {reply_markup: JSON.stringify({ inline_keyboard: playeroptions})})
+    bot.sendMessage(players[0], 'Ask <...> a Question!\nNote, you NEED to reply (tap & hold for options) to this message', opts) //ask the question
         .then(function(sent) {
-           questions[sent.message_id] = {group: chat.id , user: players[0]};
+            var chatId = sent.chat.id;
+            var messageId = sent.message_id;
+            console.log(chatId, messageId);
+            bot.onReplyToMessage(chatId, messageId, function(message) {
+                saveQuestions(message.text) // Just some NSA kinda wiretappin!
+                bot.sendMessage(chatId, 'You asked \'' + message.text + '\'');
+                bot.sendMessage(players[0], '<...> asked \'' + message.text + '\''); //TODO, REMEMBER TO SEND TO THE NEXT PLAYER
+                var playeroptions = [];
+                for (var i = 0; i < players.length; i++) {
+                    playeroptions.push([{
+                        text: compose_name(games[chat.id].players[players[i]]),
+                        callback_data: players[i]
+                    }]);
+                }
+                console.log(playeroptions);
+                bot.sendMessage(players[0], 'Imma point at: ', {
+                        reply_markup: JSON.stringify({
+                            inline_keyboard: playeroptions
+                        })
+                    })
+                    .then(function(sent) {
+                        questions[sent.message_id] = {
+                            group: chat.id,
+                            user: players[0]
+                        };
+                    });
+            });
         });
-      });
-    });
     games[chat.id].active = false; //deactivate the game for now
 });
 
-bot.on('callback_query', function (msg) {
-    if(questions[msg.message.message_id]) //it a reply to a 'select a user' question
+bot.on('callback_query', function(msg) {
+    if (questions[msg.message.message_id]) //it a reply to a 'select a user' question
     {
-      bot.editMessageText('You selected ' + compose_name(users[parseInt(msg.data)]), {chat_id: msg.message.chat.id, message_id: msg.message.message_id});
-      bot.sendMessage(questions[msg.message.message_id].group, compose_name(msg.message.chat) + ' pointed to ' + compose_name(users[parseInt(msg.data)]));
+        bot.editMessageText('You selected ' + compose_name(users[parseInt(msg.data)]), {
+            chat_id: msg.message.chat.id,
+            message_id: msg.message.message_id
+        });
+        bot.sendMessage(questions[msg.message.message_id].group, compose_name(msg.message.chat) + ' pointed to ' + compose_name(users[parseInt(msg.data)]));
     }
 });
 bot.on('message', function(msg) {
     users[msg.from.id] = msg.from;
-//  console.log(msg);
+    //  console.log(msg);
 });
 /*bot.on('message', function (msg) {
   var chatId = msg.chat.id;
